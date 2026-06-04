a2e038e9b982735b225efca243cf0fbb21b26637
erzan12 <do.earljan@gmail.com>
2026-06-04 10:10:03 +0800

    first commit: project scaffolding

---
+++ src/main/java/com/upskillingerp/spingbootauthentication/SpingBootAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.upskillingerp.spingbootauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBootAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upskillingerp/spingbootauthentication/SpingBootAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upskillingerp.spingbootauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
