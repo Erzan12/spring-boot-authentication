092638f8611a6283ae20569b30fc93997031d213
erzan12 <do.earljan@gmail.com>
2026-06-06 11:30:09 +0800

    feat: added jwt service token generator will be used when log in

--- src/main/java/com/upskillingerp/spingbootauthentication/service/JwtService.java
+++ src/main/java/com/upskillingerp/spingbootauthentication/service/JwtService.java
@@ -1,13 +1,29 @@
 package com.upskillingerp.spingbootauthentication.service;
 
+import com.upskillingerp.spingbootauthentication.entity.User;
+import com.upskillingerp.spingbootauthentication.helper.JwtHelper;
+import io.jsonwebtoken.Jwts;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+
 @Service
 public class JwtService {
+
+    // with helper configuration
+//    private final JwtHelper jwtHelper;
+
+    // with bean configuration
+    private final Key jwtSigningKey;
+
+    public JwtService(Key jwtSigningKey) {
+        this.jwtSigningKey = jwtSigningKey;
+    }
 
     private static final Logger log =
             LoggerFactory.getLogger(JwtService.class);
@@ -33,5 +49,25 @@ public class JwtService {
     public void printConfig() {
         System.out.println(secret);
         System.out.println(expiration);
+    }
+
+    public String generateToken(User user) {
+
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .issuedAt(new Date())
+                .expiration(
+                        new Date(
+                                System.currentTimeMillis()
+                                + 360000
+                        )
+                )
+                // with helper configuration
+//                .signWith(jwtHelper.getSignInKey())
+
+                // with bean configuration
+                .signWith(jwtSigningKey)
+                .compact();
     }
 }
