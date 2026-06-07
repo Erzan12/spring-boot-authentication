19f53678f4de93af9047833ce0f0d5d9dfc597c2
erzan <do.earljan@gmail.com>
2026-06-07 14:39:53 +0800

    refactor: remove unnecessary methods in security module

--- src/main/java/com/upskillingerp/spingbootauthentication/security/CustomUserDetailsService.java
+++ src/main/java/com/upskillingerp/spingbootauthentication/security/CustomUserDetailsService.java
@@ -20,26 +20,6 @@ public class CustomUserDetailsService
 //    }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
-    @Override
     public UserDetails loadUserByUsername(
             String email)
             throws UsernameNotFoundException {
