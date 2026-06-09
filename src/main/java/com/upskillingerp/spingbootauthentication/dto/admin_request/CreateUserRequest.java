d47625ddf4f5f7f17500781e9c1e8321e9c21b30
erzan12 <do.earljan@gmail.com>
2026-06-09 09:44:38 +0800

    feat: added new create user request dto for admin

---
+++ src/main/java/com/upskillingerp/spingbootauthentication/dto/admin_request/CreateUserRequest.java
@@ -0,0 +1,15 @@
+package com.upskillingerp.spingbootauthentication.dto.admin_request;
+
+import com.upskillingerp.spingbootauthentication.enums.Role;
+import lombok.Data;
+
+@Data
+public class CreateUserRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private Integer age;
+    private String address;
+    private Role role;
+}
