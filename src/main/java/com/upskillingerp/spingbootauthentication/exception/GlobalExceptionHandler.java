9790e01ff4cdec3196173d3a12c7ca1e1bf3554e
erzan12 <do.earljan@gmail.com>
2026-06-11 14:00:22 +0800

    feat: added global exception handler

---
+++ src/main/java/com/upskillingerp/spingbootauthentication/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.upskillingerp.spingbootauthentication.exception;
+
+import com.upskillingerp.spingbootauthentication.dto.api_response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.InvalidMediaTypeException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailExists(
+            EmailAlreadyExistsException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse(
+                        "EMAIL_ALREADY_EXISTS",
+                        ex.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(InvalidMediaTypeException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredentials(
+            InvalidCredentialsException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse(
+                        "INVALID_CREDENTIALS",
+                        ex.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(
+            Exception ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(
+                        "INTERNAL_SERVER_ERROR",
+                        "Something went wrong"
+                ));
+    }
+}
